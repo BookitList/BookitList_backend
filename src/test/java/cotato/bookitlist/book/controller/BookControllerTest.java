a813ede5d78a227cf5529c5f0c7cac6cafcff6d5
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-01-07 22:54:12 +0900

    #17 - test: 책 등록 테스트 추가

--- src/test/java/cotato/bookitlist/book/controller/BookControllerTest.java
+++ src/test/java/cotato/bookitlist/book/controller/BookControllerTest.java
@@ -1,16 +1,19 @@
 package cotato.bookitlist.book.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cotato.bookitlist.book.dto.request.BookRegisterRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,6 +24,9 @@ class BookControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     @DisplayName("keyword가 주어지면 응답을 준다.")
     void givenKeyword_whenExternalSearching_thenReturnBookApiResponse() throws Exception {
@@ -29,14 +35,62 @@ class BookControllerTest {
 
         //when&then
         mockMvc.perform(get("/books/external")
-                        .param("key-word", keyword)
+                        .param("keyword", keyword)
                         .param("start", "1")
                 )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.totalResults").exists())
                 .andExpect(jsonPath("$.startIndex").exists())
                 .andExpect(jsonPath("$.itemsPerPage").exists())
-                .andExpect(jsonPath("$.bookApiDto").exists());
+                .andExpect(jsonPath("$.bookApiDto").exists())
+        ;
+    }
+
+    @Test
+    @DisplayName("isbn13을 통해 책을 등록한다.")
+    void givenIsbn13_whenRegisteringBook_thenReturnBookResponse() throws Exception {
+        //given
+        BookRegisterRequest request = new BookRegisterRequest("9791197045318");
+
+        //when&then
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsBytes(request))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+        ;
+    }
+
+    @Test
+    @DisplayName("이미 등록된 책을 등록하면 에러를 반환한다.")
+    void givenRegisteredIsbn13_whenRegisteringBook_thenReturnErrorResponse() throws Exception {
+        //given
+        BookRegisterRequest request = new BookRegisterRequest("9788931514803");
+
+        //when&then
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsBytes(request))
+                )
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("message").value("이미 등록된 isbn13입니다."))
+        ;
+    }
+
+    @Test
+    @DisplayName("잘못된 형식의 isbn13으로 등록하면 에러를 반환한다.")
+    void givenInvalidIsbn13_whenRegisteringBook_thenReturnErrorResponse() throws Exception {
+        //given
+        BookRegisterRequest request = new BookRegisterRequest("9788931514invalid");
+
+        //when&then
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsBytes(request))
+                )
+                .andExpect(status().isBadRequest())
+        ;
     }
 
 }

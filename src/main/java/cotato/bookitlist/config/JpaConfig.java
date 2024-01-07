bac61e4305a32504211d01982d49ae3c02514868
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-01-07 23:02:33 +0900

    #17 - feat: `@EnableJpaAuditing` 설정 추가

---
+++ src/main/java/cotato/bookitlist/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package cotato.bookitlist.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}

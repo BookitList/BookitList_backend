f35226282d0dc7a4cb480db51dd33a9486fcb201
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-01-10 10:21:39 +0900

    #20 - docs: `TODO` 추가

--- src/main/java/cotato/bookitlist/book/repository/BookRepository.java
+++ src/main/java/cotato/bookitlist/book/repository/BookRepository.java
@@ -12,6 +12,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findByIsbn13(String isbn13);
 
+    // TODO: 단순 like query로 한영에 따라 결과가 다르게 나온다. 이를 해결해야함!!
     @Query("SELECT b FROM Book b WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(b.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     Page<Book> findAllByKeyword(String keyword, Pageable pageable);
 }

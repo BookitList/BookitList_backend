44109e1b5f3354f955dd6ef6c27498f8a61b2e0d
morenow <jjang9877@gmail.com>
2024-01-15 18:35:11 +0900

    * #11 - feat: 인증 중 유저 정보에 트랜잭션 처리

--- src/main/java/cotato/bookitlist/config/security/oauth/service/CustomOAuth2UserService.java
+++ src/main/java/cotato/bookitlist/config/security/oauth/service/CustomOAuth2UserService.java
@@ -13,9 +13,11 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final MemberRepository memberRepository;
 

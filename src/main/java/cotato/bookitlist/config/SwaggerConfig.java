dd2e13f8301256831ca6e9c113f6fc7c135b219e
morenow98 <112200350+morenow98@users.noreply.github.com>
2024-01-17 22:49:37 +0900

    Feat: 로컬과 배포 환경에 따른 Swagger 설정, Swagger에서의 Authorization 설정 (#33)

---
+++ src/main/java/cotato/bookitlist/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package cotato.bookitlist.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://api.bookitlist.store").description("Dev Server"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Local Server"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                );
+    }
+}

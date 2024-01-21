3b99fcab7763a0aa0d98bd27177874d327c58b66
morenow <jjang9877@gmail.com>
2024-01-22 02:40:20 +0900

    * #25 - feat: Refresh Token 을 Redis 에 저장하도록 domain, repository 구현

---
+++ src/main/java/cotato/bookitlist/auth/domain/RefreshTokenEntity.java
@@ -0,0 +1,33 @@
+package cotato.bookitlist.auth.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@RedisHash(value = "refreshTokenEntity")
+public class RefreshTokenEntity {
+
+    @Id
+    private Long id;
+
+    @Indexed
+    private String refreshToken;
+
+    @TimeToLive // TTL
+    private Long ttl;
+
+    @Builder
+    public RefreshTokenEntity(Long id, String refreshToken, Long ttl) {
+        this.id = id;
+        this.refreshToken = refreshToken;
+        this.ttl = ttl;
+    }
+
+    public void updateTTL(Long ttl) {
+        this.ttl += ttl;
+    }
+}

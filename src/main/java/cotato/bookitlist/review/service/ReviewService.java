9b4856fc9519a0887963f8ed782d04c0626008ef
morenow <jjang9877@gmail.com>
2024-01-29 19:12:52 +0900

    #40 - refactor: AuthDetails 에서 찾는 멤버를 getReferenceById() 로 변경

--- src/main/java/cotato/bookitlist/review/service/ReviewService.java
+++ src/main/java/cotato/bookitlist/review/service/ReviewService.java
@@ -6,14 +6,11 @@ import cotato.bookitlist.member.domain.Member;
 import cotato.bookitlist.member.repository.MemberRepository;
 import cotato.bookitlist.review.domain.Review;
 import cotato.bookitlist.review.dto.request.ReviewRegisterRequest;
-import cotato.bookitlist.review.dto.response.ReviewResponse;
 import cotato.bookitlist.review.repository.ReviewRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
@@ -25,9 +22,8 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
 
     public Long registerReview(ReviewRegisterRequest request, Long memberId) {
+        Member member = memberRepository.getReferenceById(memberId);
 
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 멤버입니다."));
         Book book = bookRepository.findById(request.bookId())
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 책입니다."));
 

34862666938785dad444d0599b3f6808ab9b7345
morenow98 <112200350+morenow98@users.noreply.github.com>
2024-02-05 00:19:42 +0900

    Feat: 책 상세 API 접근 시 DB에 없는 책도 가능한 기능 추가 (#68)
    * #53 - feat: 형식은 올바르지만 실제로는 없는 isbn13 입력 시 예외처리
    
    * #53 - feat: 책 상세 API 중 데이터베이스에 존재하지 않을 때 에러를 반환하지 않고 외부 API 통신을 통해 보여주도록 변경
    
    관련된 테스트 코드 수정
    DB에 없는 책일 경우 bookId 를 0으로 반환
    
    * #53 - feat: 책 상세 조회 시 DB 조회 -> Redis 조회 -> API 통신으로 데이터를 얻어오게 변경
    
    * #53 - refactor: 변경된 메소드의 메소드명 수정 누락분 변경

--- src/main/java/cotato/bookitlist/book/dto/BookDto.java
+++ src/main/java/cotato/bookitlist/book/dto/BookDto.java
@@ -18,9 +18,28 @@ public record BookDto(
 ) {
 
     public static BookDto from(Book entity) {
-        return new BookDto(entity.getId(), entity.getTitle(), entity.getAuthor(), entity.getPublisher(), entity.getPubDate(), entity.getDescription(), entity.getLink(), entity.getIsbn13(), entity.getPrice(), entity.getCover());
-
+        return new BookDto(entity.getId(),
+                entity.getTitle(),
+                entity.getAuthor(),
+                entity.getPublisher(),
+                entity.getPubDate(),
+                entity.getDescription(),
+                entity.getLink(),
+                entity.getIsbn13(),
+                entity.getPrice(),
+                entity.getCover());
     }
 
-
+    public static BookDto from(BookApiDto bookApiDto) {
+        return new BookDto(0L,
+                bookApiDto.title(),
+                bookApiDto.author(),
+                bookApiDto.publisher(),
+                bookApiDto.pubDate(),
+                bookApiDto.description(),
+                bookApiDto.link(),
+                bookApiDto.isbn13(),
+                bookApiDto.price(),
+                bookApiDto.cover());
+    }
 }

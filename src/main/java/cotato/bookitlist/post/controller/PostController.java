b0dc1494e94a3ac537fca6d8c05358f06f89a0e4
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-02-06 16:14:43 +0900

    #76 - feat: 게시글 상세 조회시 조회수 증가 기능 구현
    쿠키를 사용하여 중복된 조회수 증가를 방지한다.

--- src/main/java/cotato/bookitlist/post/controller/PostController.java
+++ src/main/java/cotato/bookitlist/post/controller/PostController.java
@@ -8,6 +8,9 @@ import cotato.bookitlist.post.dto.response.PostCountResponse;
 import cotato.bookitlist.post.dto.response.PostListResponse;
 import cotato.bookitlist.post.dto.response.PostResponse;
 import cotato.bookitlist.post.service.PostService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -25,6 +28,7 @@ import java.net.URI;
 @RequestMapping("/posts")
 public class PostController {
     private static final Long DEFAULT_USER_ID = 0L;
+    private static final String POST_VIEW_COOKIE_NAME = "post_view";
 
     private final PostService postService;
 
@@ -57,13 +61,20 @@ public class PostController {
 
     @GetMapping("/{post-id}")
     public ResponseEntity<PostResponse> getPost(
+            HttpServletRequest request,
+            HttpServletResponse response,
             @PathVariable("post-id") Long postId,
             @AuthenticationPrincipal AuthDetails details
     ) {
+        ResponseEntity<PostResponse> responseEntity;
         if (details == null) {
-            return ResponseEntity.ok(PostResponse.from(postService.getPost(postId, DEFAULT_USER_ID), DEFAULT_USER_ID));
+            responseEntity = ResponseEntity.ok(PostResponse.from(postService.getPost(postId, DEFAULT_USER_ID), DEFAULT_USER_ID));
+        } else {
+            responseEntity = ResponseEntity.ok(PostResponse.from(postService.getPost(postId, details.getId()), details.getId()));
         }
-        return ResponseEntity.ok(PostResponse.from(postService.getPost(postId, details.getId()), details.getId()));
+
+        handlePostViewCount(request, response, postId);
+        return responseEntity;
     }
 
     @GetMapping("/all")
@@ -91,5 +102,35 @@ public class PostController {
             @IsValidIsbn @RequestParam String isbn13
     ) {
         return ResponseEntity.ok(postService.getPostCount(isbn13));
+    }
+
+    private void handlePostViewCount(HttpServletRequest request, HttpServletResponse response, Long postId) {
+        Cookie[] cookies = request.getCookies();
+        Cookie postViewCookie = findCookie(cookies);
+
+        if (postViewCookie != null) {
+            if (!postViewCookie.getValue().contains("[" + postId + "]")) {
+                postViewCookie.setValue(postViewCookie.getValue() + "[" + postId + "]");
+                postService.increaseViewCount(postId);
+                postViewCookie.setPath("/posts");
+            }
+            response.addCookie(postViewCookie);
+        } else {
+            Cookie newCookie = new Cookie(POST_VIEW_COOKIE_NAME, "[" + postId + "]");
+            newCookie.setPath("/posts");
+            postService.increaseViewCount(postId);
+            response.addCookie(newCookie);
+        }
+    }
+
+    private Cookie findCookie(Cookie[] cookies) {
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (POST_VIEW_COOKIE_NAME.equals(cookie.getName())) {
+                    return cookie;
+                }
+            }
+        }
+        return null;
     }
 }

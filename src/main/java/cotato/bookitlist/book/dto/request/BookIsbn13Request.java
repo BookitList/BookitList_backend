1a2d7dd1b7c18e18eb5faad9c3f10d8ea5fa9402
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-02-08 21:24:18 +0900

    #94 - refactor: Requset Dto 이름 변경
    등록시 말고도 사용하기 위해

--- src/main/java/cotato/bookitlist/book/dto/request/BookIsbn13Request.java
+++ src/main/java/cotato/bookitlist/book/dto/request/BookIsbn13Request.java
@@ -3,7 +3,7 @@ package cotato.bookitlist.book.dto.request;
 
 import cotato.bookitlist.book.annotation.IsValidIsbn;
 
-public record BookRegisterRequest(
+public record BookIsbn13Request(
         @IsValidIsbn
         String isbn13
 ) {
